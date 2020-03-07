e8fdfd0f7a874fb7116dc0dfbbb4ae8c873422cf
Thokas <2893148+Thokas@users.noreply.github.com>
2020-03-07 06:02:04 +0100

    change to make google config optional

--- routes/homepage.js
+++ routes/homepage.js
@@ -9,7 +9,7 @@ exports.index = function(req, res){
   res.render('index', {
       title: "Home",
       user: req.user,
-      google: config.google,
+      google: (config.google) ? config.google : {},
       errormessages: errormessages,
       infomessages: infomessages
   });
@@ -22,7 +22,7 @@ exports.docs = function(req, res) {
     res.render('docs/documentation', {
         title: "Docs",
         user: req.user,
-        google: config.google,
+        google: (config.google) ? config.google : {},
         errormessages: errormessages,
         infomessages: infomessages
     });
@@ -35,7 +35,7 @@ exports.docsnotifications = function(req, res) {
     res.render('docs/notifications', {
         title: "Docs - Notifications",
         user: req.user,
-        google: config.google,
+        google: (config.google) ? config.google : {},
         errormessages: errormessages,
         infomessages: infomessages
     });
@@ -48,7 +48,7 @@ exports.docspersistence = function(req, res) {
     res.render('docs/persistence', {
         title: "Docs - Persistence",
         user: req.user,
-        google: config.google,
+        google: (config.google) ? config.google : {},
         errormessages: errormessages,
         infomessages: infomessages
     });
@@ -61,7 +61,7 @@ exports.docsifttt = function(req, res) {
     res.render('docs/ifttt', {
         title: "Docs - IFTTT",
         user: req.user,
-        google: config.google,
+        google: (config.google) ? config.google : {},
         errormessages: errormessages,
         infomessages: infomessages
     });
