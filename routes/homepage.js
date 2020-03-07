f954172dc9f81519db1cf8f13bf88a7a6e4372f6
Thokas <2893148+Thokas@users.noreply.github.com>
2020-03-07 05:18:03 +0100

    add google config to views

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,38 +3,68 @@
  * GET home page.
  */
 exports.index = function(req, res){
-  errormessages = req.flash('error');
-  infomessages = req.flash('info');
-  res.render('index', {title: "Home", user: req.user, errormessages: errormessages,
-    infomessages: infomessages});
+  const errormessages = req.flash('error');
+  const infomessages = req.flash('info');
+  const config = require('../config.json');
+  res.render('index', {
+      title: "Home",
+      user: req.user,
+      google: config.google,
+      errormessages: errormessages,
+      infomessages: infomessages
+  });
 };
 
 exports.docs = function(req, res) {
-    errormessages = req.flash('error');
-    infomessages = req.flash('info');
-    res.render('docs/documentation', {title: "Docs", user: req.user, errormessages: errormessages,
-        infomessages: infomessages});
+    const errormessages = req.flash('error');
+    const infomessages = req.flash('info');
+    const config = require('../config.json');
+    res.render('docs/documentation', {
+        title: "Docs",
+        user: req.user,
+        google: config.google,
+        errormessages: errormessages,
+        infomessages: infomessages
+    });
 };
 
 exports.docsnotifications = function(req, res) {
-    errormessages = req.flash('error');
-    infomessages = req.flash('info');
-    res.render('docs/notifications', {title: "Docs - Notifications", user: req.user, errormessages: errormessages,
-        infomessages: infomessages});
+    const errormessages = req.flash('error');
+    const infomessages = req.flash('info');
+    const config = require('../config.json');
+    res.render('docs/notifications', {
+        title: "Docs - Notifications",
+        user: req.user,
+        google: config.google,
+        errormessages: errormessages,
+        infomessages: infomessages
+    });
 };
 
 exports.docspersistence = function(req, res) {
-    errormessages = req.flash('error');
-    infomessages = req.flash('info');
-    res.render('docs/persistence', {title: "Docs - Persistence", user: req.user, errormessages: errormessages,
-        infomessages: infomessages});
+    const errormessages = req.flash('error');
+    const infomessages = req.flash('info');
+    const config = require('../config.json');
+    res.render('docs/persistence', {
+        title: "Docs - Persistence",
+        user: req.user,
+        google: config.google,
+        errormessages: errormessages,
+        infomessages: infomessages
+    });
 };
 
 exports.docsifttt = function(req, res) {
-    errormessages = req.flash('error');
-    infomessages = req.flash('info');
-    res.render('docs/ifttt', {title: "Docs - IFTTT", user: req.user, errormessages: errormessages,
-        infomessages: infomessages});
+    const errormessages = req.flash('error');
+    const infomessages = req.flash('info');
+    const config = require('../config.json');
+    res.render('docs/ifttt', {
+        title: "Docs - IFTTT",
+        user: req.user,
+        google: config.google,
+        errormessages: errormessages,
+        infomessages: infomessages
+    });
 };
 
 exports.getv2 = function(req, res) {
